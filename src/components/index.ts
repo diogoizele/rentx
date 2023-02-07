935a143e68dc62b5e3fdb618ddf9f5b12e0d4e1c
diogoizele <diogogabrielizele@gmail.com>
2023-02-07 11:04:15 -0300

    feat: add index exporter file to components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,6 @@
 export { Car } from "./Car/Car.component";
 export { BackButton } from "./BackButton/BackButton.component";
+export { Accessory } from "./Accessory/Accessory.component";
+export { ImageSlider } from "./ImageSlider/ImageSlider.component";
+export { Button } from "./Button/Button.component";
+export { Calendar } from "./Calendar/Calendar.component";
