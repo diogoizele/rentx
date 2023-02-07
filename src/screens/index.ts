28a7ff4d097fcca7b9a928973d538336335551ac
diogoizele <diogogabrielizele@gmail.com>
2023-02-07 11:04:25 -0300

    feat: add index exporter file to screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,2 +1,4 @@
 export { Home } from "./Home/Home.screen";
 export { CarDetails } from "./CarDetails/CarDetails.screen";
+export { Scheduling } from "./Scheduling/Scheduling.screen";
+export { SchedulingDetails } from "./SchedulingDetails/SchedulingDetails.screen";
